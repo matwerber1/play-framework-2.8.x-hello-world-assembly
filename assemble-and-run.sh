fb9487c5468b2a093b80b9249cb611f77d997f2a
Mat Werber <matwerber@gmail.com>
2020-12-16 15:45:00 -0800

    add ability to skip assembly

--- assemble-and-run.sh
+++ assemble-and-run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -xe
 
-DIR="play-java-hello-world-tutorial"
-JAR_PATH="$DIR/target/scala-2.13/play-java-hello-world-tutorial-assembly-1.0-SNAPSHOT.jar"
+DIR="play-java-hello-world-tutorial/target/scala-2.13"
+JAR_PATH="$DIR/play-java-hello-world-tutorial-assembly-1.0-SNAPSHOT.jar"
 #SECRET=-"Dplay.http.secret.key=ad31779d4ee49d5ad5162bf1429c32e2e9933f3b"
 #PORT="-Dhttp.port=9000 -Dhttp.address=127.0.0.1"
-#SERVER_PATH="-Dplay.server.dir=."
+#SERVER_PATH="-Dplay.server.dir=$DIR"
 #PID_PATH="-Dpidfile.path=/var/run/play.pid"
 
 #JAR_PATH="play-java-hello-world-tutorial-assembly-1.0-SNAPSHOT.jar"
 
+if [ "$1" -ne "--no-assembly" ]
+then
+    (cd play-java-hello-world-tutorial && sbt assembly)
+fi
 
-(cd $DIR && sbt assembly)
-
-java -jar $JAR_PATH
+java $SERVER_PATH -jar $JAR_PATH
